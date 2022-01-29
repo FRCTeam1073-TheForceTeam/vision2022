2a4e977819c0ef4888b7fba78f2b4d8763e3a595
TomP6 <thomas.p@frc1073.org>
2022-01-29 12:00:22 -0500

    made mask Image

--- src/main/java/CargoTracker.java
+++ src/main/java/CargoTracker.java
@@ -2,10 +2,12 @@ import edu.wpi.first.vision.VisionPipeline;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.features2d.SimpleBlobDetector;
 import org.opencv.imgproc.*;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
@@ -20,6 +22,7 @@ public class CargoTracker implements VisionPipeline {
     private NetworkTableEntry cargoY;
     private NetworkTableEntry cargoArea;
     private CvSource output;
+    //private SimpleBlobDetectorParams blobParam
 
     public CargoTracker(NetworkTableInstance ntinst, CvSource output_){
         nti = ntinst;
@@ -38,9 +41,14 @@ public class CargoTracker implements VisionPipeline {
     public void process(Mat inputImage) {
       frameCounter += 1;
      
+      Mat labImage = new Mat();
+      Mat maskImage = new Mat();
+      Imgproc.cvtColor(inputImage, labImage, Imgproc.COLOR_BGR2Lab);
+      Core.inRange(labImage, new Scalar(20, 50, 50), new Scalar(240, 100, 100), maskImage);
       Mat outputImage = new Mat();
-      Imgproc.Sobel(inputImage, outputImage, -1, 1, 1);
-      Imgproc.line(outputImage, new Point(0, outputImage.rows()/2), new Point(outputImage.cols()-1, outputImage.rows()/2), new Scalar(0, 0, 255));
+      Core.bitwise_and(inputImage, inputImage, outputImage, maskImage);
+     // Imgproc.Sobel(inputImage, outputImage, -1, 1, 1);
+      //Imgproc.line(outputImage, new Point(0, outputImage.rows()/2), new Point(outputImage.cols()-1, outputImage.rows()/2), new Scalar(0, 0, 255));
       output.putFrame(outputImage);
     }
   }
