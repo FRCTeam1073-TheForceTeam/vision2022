1910e4d6c3f03e64110207ca13a6a0dac63b69fa
TomP6 <thomas.p@frc1073.org>
2022-03-07 20:57:10 -0500

    Update HubTracker.java
    Updated HSV values to sense green
    drew lines on image to show hub positions
    drew crosshairs on best rectangle

--- src/main/java/HubTracker.java
+++ src/main/java/HubTracker.java
@@ -53,17 +53,17 @@ public class HubTracker implements VisionPipeline {
         hubArea = hubTable.getEntry("Hub Area");
         hubArea.setDouble(0);
         hubHMin = hubTable.getEntry("H Min");
-        hubHMin.setDouble(0);
+        hubHMin.setDouble(110);
         hubHMax = hubTable.getEntry("H Max");
-        hubHMax.setDouble(30);
+        hubHMax.setDouble(140);
         hubSMin = hubTable.getEntry("S Min");
         hubSMin.setDouble(90);
         hubSMax = hubTable.getEntry("S Max");
         hubSMax.setDouble(255);
         hubVMin = hubTable.getEntry("V Min");
-        hubVMin.setDouble(60);
+        hubVMin.setDouble(80);
         hubVMax = hubTable.getEntry("V Max");
-        hubVMax.setDouble(252);
+        hubVMax.setDouble(255);
         saveHubImage = hubTable.getEntry("Save Hub Images");
         saveHubImage.setBoolean(false);
 
@@ -81,9 +81,10 @@ public class HubTracker implements VisionPipeline {
       frameCounter += 1;
 
       Imgproc.cvtColor(inputImage, hsvImage, Imgproc.COLOR_BGR2HSV_FULL);
-      Core.inRange(hsvImage, new Scalar(hubHMin.getDouble(0), hubSMin.getDouble(50), hubVMin.getDouble(20)), new Scalar(hubHMax.getDouble(30), hubSMax.getDouble(250), hubVMax.getDouble(240)), maskImage);
-      outputImage.setTo(new Scalar(0,0,0));
-      Core.bitwise_and(inputImage, inputImage, outputImage, maskImage);
+      Core.inRange(hsvImage, new Scalar(hubHMin.getDouble(110), hubSMin.getDouble(90), hubVMin.getDouble(80)), new Scalar(hubHMax.getDouble(140), hubSMax.getDouble(255), hubVMax.getDouble(255)), maskImage);
+     // outputImage.setTo(new Scalar(0,0,0));
+      Imgproc.resize(inputImage, outputImage, new Size(inputImage.cols()/2, inputImage.rows()/2));
+     // Core.bitwise_and(inputImage, inputImage, outputImage, maskImage);
 
       // Erode the mask image to eliminate the little "noise" pixels
       Imgproc.erode(maskImage, maskImage, erosionKernel);
@@ -102,17 +103,17 @@ public class HubTracker implements VisionPipeline {
         // Grab the bounding rectangle for contour:
         Rect bounds = Imgproc.boundingRect(contours.get(cidx));
         double aspecterr = Math.abs(1.0 - (double)bounds.width/(double)bounds.height);
-        Imgproc.rectangle(outputImage, bounds.br(), bounds.tl(), new Scalar(255,0,0));
+      //  Imgproc.rectangle(outputImage, bounds.br(), bounds.tl(), new Scalar(255,0,0));
 
         // Only draw contours that have nearly square bounding boxes, and some minimal area... like round things.
        // if (bounds.area() > 32 && aspecterr < 0.3) {
           // Now we know it has non-trivial size and is closer to square/round:
 
-          Imgproc.drawContours(outputImage, contours, cidx, new Scalar(0, 255, 0));
+       //   Imgproc.drawContours(outputImage, contours, cidx, new Scalar(0, 255, 0));
          if (bounds.area() > bestArea){
            bestArea = bounds.area();
-           bestX = bounds.x;
-           bestY = bounds.y;
+           bestX = bounds.x + bounds.width/2;
+           bestY = bounds.y + bounds.height/2;
          }
         }
         // }
@@ -127,6 +128,19 @@ public class HubTracker implements VisionPipeline {
 
       }
 
+      // Line for 1 meter
+      Imgproc.line(outputImage, new Point(0, 6/2), new Point(outputImage.cols()-1, 6/2), new Scalar(0, 0, 255));
+      // Line for 2 meters
+      Imgproc.line(outputImage, new Point(0, 191/2), new Point(outputImage.cols()-1, 191/2), new Scalar(0, 0, 255));
+      // Line for 3 meters
+      Imgproc.line(outputImage, new Point(0, 299/2), new Point(outputImage.cols()-1, 299/2), new Scalar(0, 0, 255));
+      // Line for 4 meters
+      Imgproc.line(outputImage, new Point(0, 372/2), new Point(outputImage.cols()-1, 372/2), new Scalar(0, 0, 255));
+      // Line for 5 meters
+      Imgproc.line(outputImage, new Point(0, 423/2), new Point(outputImage.cols()-1, 423/2), new Scalar(0, 0, 255));
+
+      Imgproc.line(outputImage, new Point(bestX/2, 0), new Point(bestX/2, outputImage.rows()-1), new Scalar(0, 255, 0));
+      Imgproc.line(outputImage, new Point(0, bestY/2), new Point(outputImage.cols()-1, bestY/2), new Scalar(0, 255, 0));
 
      // Imgproc.Sobel(inputImage, outputImage, -1, 1, 1);
       //Imgproc.line(outputImage, new Point(0, outputImage.rows()/2), new Point(outputImage.cols()-1, outputImage.rows()/2), new Scalar(0, 0, 255));
